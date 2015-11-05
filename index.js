67dcd04ccff0834408c09385f1696100671257b0
Liam Childs <liam.childs91@gmail.com>
2015-11-05 18:31:56 +0000

    Added selection framework and tournament selection

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 var lib = require('./lib');
 
-var options = {
-    generations: 1000,
-    populationSize: 30,
-    elites: 1,
-    mutationRate: 0.05
+var combination = function(ind1, ind2) {
+    return {
+        x: (ind1.x + ind2.x) / 2,
+        y: (ind1.y + ind2.y) / 2
+    }
 };
+
+var mutation = function(ind) {
+  return {
+      x: 'random', // function(attr)
+      y: 'random'
+  };
+};
+
+var options = {
+    generations: 3,
+    populationSize: 10,
+    elites: 1,
+    mutationRate: 0.05,
+    breeding: {
+        selectionOne: 'tournament',
+        selectionTwo: 'tournament',
+        combinationFunction: combination,
+        mutationFunction: mutation
+    }
+};
+
 
 var individual = {
     x: {
@@ -18,7 +39,8 @@ var individual = {
     }
 };
 
-function problem(individual) {}
+function problem(individual) {
+}
 
 
 var evolve = lib();
@@ -26,6 +48,5 @@ var evolve = lib();
 evolve.setConfig(options);
 evolve.setIndividual(individual);
 //evolve.setProblem(problem);
-//evolve.setPipeline();
 
 evolve.run();
